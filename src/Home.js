900a8d051df2a54f3cdaf771316880c4fcc9b178
Manish <manish.sharma@ebizontek.com>
2021-07-07 16:29:28 +0530

    usage of use effect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -12,6 +12,10 @@ const Home = () => {
         const newBlogs = blogs.filter((a) => a.id !== id);
         setBlogs(newBlogs);
     }
+    useEffect(() => {
+        console.log("use effect run");
+        console.log(blogs)
+    })
     return (
         <div className="home">
             <BlogList blogs={blogs} title={"All Blogs"} handleDelete={handleDelete} />
