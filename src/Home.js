4fb63117de6550535ced6d1ce7f068e22e8be515
Manish <manish.sharma@ebizontek.com>
2021-07-07 16:24:14 +0530

    function as a prop

--- src/Home.js
+++ src/Home.js
@@ -7,10 +7,14 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((a) => a.id !== id);
+        setBlogs(newBlogs);
+    }
     return (
         <div className="home">
-            <BlogList blogs={blogs} title={"All Blogs"} />
-            <BlogList blogs={blogs.filter((b) => b.author == 'mario' )} title={"Mario's Blogs"} />
+            <BlogList blogs={blogs} title={"All Blogs"} handleDelete={handleDelete} />
         </div>
     );
 }
