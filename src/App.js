8b1e181a01bebb19227977460baf241bb9291eb5
Manish <manish.sharma@ebizontek.com>
2021-07-08 14:17:51 +0530

    react routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 
 import Navbar from './Navbar';
 import Home from './Home';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 function App() {
   return (
-    <div className="App">
+    <Router>
+<div className="App">
       <Navbar />
       <div className="content">
-       <Home />
+      <Switch>
+      <Route exact path="/create" >
+        <Create />
+        </Route>
+        <Route exact path="/" >
+        <Home />
+        </Route>        
+      </Switch>
       </div>
     </div>
+    </Router>    
   );
 }
 
